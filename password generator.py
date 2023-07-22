5f44e6870a90ff742bf12cbff88cb8ccb70e8b4d
Sondre Indset <sondre2004@hotmail.no>
2023-07-22 15:57:10 +0200

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,25 @@
+import random as rd
+
+def generer_passord():
+    små_bokstaver = "abcdefghijklmnopqrstuvwxyzæøå"
+    store_bokstaver = "ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ"
+    tegn = "!#$%&/=?+"
+    alle_tegn = små_bokstaver + store_bokstaver + tegn
+    lengde = 19
+
+    passord = ""
+    teller = 0
+
+    for x in range(lengde):
+        if teller < 4:
+            tilfeldig = rd.randint(0, len(alle_tegn)-1)
+            passord += alle_tegn[tilfeldig]
+            teller += 1
+        else:
+            passord += "-"
+            teller = 0
+        
+    print("\nDitt nye passord er: ")
+    print(passord + "\n")
+
+generer_passord()
